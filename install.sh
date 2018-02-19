a428ba0d274e587ce6d317f72900c6a0e8dc927a
M Starch <lestarch@googlemail.com>
2018-02-19 10:43:05 -0800

    lestarch: adding in backup camera support

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+##
+# Settings
+##
+KODI=/home/osmc/.kodi/userdata/
+HERE=`dirname $0`
+HERE=`cd ${HERE}; pwd`
+##
+# Sediq required packages
+##
+sudo apt-get install \
+  build-essential \
+  gcc \
+  automake \
+  git \
+  vim \
+  gstreamer-1.0 \
+  gstreamer-tools \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  libgtk2.0-dev \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  rbp-userland-dev-osmc
+##
+# WGET the source for OMX acceleration
+##
+wget https://gstreamer.freedesktop.org/src/gst-omx/gst-omx-1.10.4.tar.xz
+tar -xJf gst-omx-1.10.4.tar.xz
+cd gst-omx-1.10.4
+./configure --with-omx-header-path=/opt/vc/include/IL --with-omx-target=rpi
+make -j4
+#sudo make install
+##
+# Link in the installed library
+##
+OCWD=`pwd`
+cd /usr/lib/arm-linux-gnueabihf/gstreamer-1.0/
+sudo ln -s /usr/local/lib/gstreamer-1.0/libgstomx.so
+cd ${OCWD}
+##
+# Aux video files
+##
+cd ${KODI}
+ln -s ${HERE}/camera/advancedsettings.xml
+cd ${OCWD}
